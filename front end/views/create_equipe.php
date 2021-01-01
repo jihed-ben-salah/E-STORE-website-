82fb330352f18de2feb303d13b46c07c43adcdfe
zeineb400 <bouayedzayneb@gmail.com>
2021-01-01 23:05:18 +0100

    creation d'une equipe

---
+++ front end/views/create_equipe.php
@@ -0,0 +1,39 @@
+<!doctype html>
+ <html lang="en">
+
+ <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <link href="assets/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="assets/fontawesome-all.css">
+    <title>Gerer votre compte  </title>
+
+ </head>
+    <body>
+
+<div>
+     <form class="needs-validation" action="ajoutE.php" method="POST" >
+     <div class="mb-3">
+                                                    <label for="nom_equipe">equipe</label>
+                                                    <div class="input-group">
+
+                                                        <input type="text" name="nom_equipe" class="form-control" id="equipe" placeholder="equipe "  required="">
+
+                                                    </div>
+                                                </div>                                         
+
+
+
+                                                <br>
+                                                <button class="btn btn-primary btn-lg btn-block" type="submit">Ajouter Maintenant</button>
+
+                                                 
+        </form>
+        </div>
+
+    </body>
+</html>
